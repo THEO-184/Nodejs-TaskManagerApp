6946bade7fc66addc244a12f1d89be61b9f0f438
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-05-21 16:08:13 +0000

    create midlweare wrapper for controllers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const tasksRouter = require("./routes/tasks");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleWare = require("./middleware/errorHandler");
 
 // serve static files
 app.use(express.static("./public"));
@@ -10,16 +12,20 @@ app.use(express.static("./public"));
 app.use(express.json());
 // routes
 app.use("/api/v1/tasks", tasksRouter);
+app.use(notFound);
+app.use(errorHandlerMiddleWare);
 
 const port = 3000;
 
 const start = async () => {
 	try {
-		await connectDB(process.env.MONGO_URI);
+		await connectDB(process.env.DB_URL);
 		app.listen(port, () => {
 			console.log(`server running on port ${port}`);
 		});
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
--- controller/tasks.js
+++ controller/tasks.js
@@ -1,70 +1,53 @@
 const Task = require("../model/Tasks");
+const asyncWrapper = require("../middleware/async-wrapper");
 
-const getAllTasks = async (req, res) => {
-	try {
-		const tasks = await Task.find({});
-		res.status(200).json({ tasks });
-	} catch (error) {
-		res.status(500).json({ msg: error.errors });
-	}
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+	const tasks = await Task.find({});
+	res.status(200).json({ success: true, total: tasks.length, tasks });
+});
 
-const createTask = async (req, res) => {
-	try {
-		const task = await Task.create(req.body);
-		res.status(201).json({ task });
-	} catch (error) {
-		res.status(500).json({ msg: error.errors });
-	}
-};
+const createTask = asyncWrapper(async (req, res) => {
+	const task = await Task.create(req.body);
+	res.status(201).json({ success: true, task });
+});
 
-const getSingleTask = async (req, res) => {
-	try {
-		const { id: taskID } = req.params;
-		const singleTask = await Task.findOne({ _id: taskID });
-		if (!singleTask) {
-			return res.status(404).json({ msg: `No task with id : ${taskID}` });
-		}
-		res.status(200).json({ task: singleTask });
-	} catch (error) {
-		res.status(500).json({ msg: error.errors });
+const getSingleTask = asyncWrapper(async (req, res) => {
+	const { id: taskID } = req.params;
+	const singleTask = await Task.findOne({ _id: taskID });
+	if (!singleTask) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `No task with id : ${taskID}` });
 	}
-};
+	res.status(200).json({ success: true, task: singleTask });
+});
 
-const updateSingleTasks = async (req, res) => {
-	try {
-		const { id: taskID } = req.params;
-		const query = { _id: taskID };
-		const task = await Task.findOneAndUpdate(query, req.body, {
-			new: true,
-			runValidators: true,
-		});
-		if (!task) {
-			return res
-				.status(404)
-				.json({ success: false, msg: `No task with id ${taskID}` });
-		}
-		res.status(200).json({ task });
-	} catch (error) {
-		res.status(500).json({ msg: error.errors });
+const updateSingleTasks = asyncWrapper(async (req, res) => {
+	const { id: taskID } = req.params;
+	const query = { _id: taskID };
+	const task = await Task.findOneAndUpdate(query, req.body, {
+		new: true,
+		runValidators: true,
+	});
+	if (!task) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `No task with id ${taskID}` });
 	}
-};
+	res.status(200).json({ success: true, task });
+});
 
-const deteleSingleTask = async (req, res) => {
-	try {
-		const { id: taskID } = req.params;
-		const task = await Task.findOneAndDelete({ _id: taskID });
-		res.status(200).json({
-			success: true,
-			msg: `task with id: ${taskID} successfully deleted`,
-		});
-		if (!task) {
-			return res.status(404).json({ msg: `No task with id: ${taskID}` });
-		}
-	} catch (error) {
-		res.status(500).json({ msg: error.errors });
+const deteleSingleTask = asyncWrapper(async (req, res) => {
+	const { id: taskID } = req.params;
+	const task = await Task.findOneAndDelete({ _id: taskID });
+	res.status(200).json({
+		success: true,
+		msg: `task with id: ${taskID} successfully deleted`,
+	});
+	if (!task) {
+		return res.status(404).json({ msg: `No task with id: ${taskID}` });
 	}
-};
+});
 
 module.exports = {
 	getAllTasks,
